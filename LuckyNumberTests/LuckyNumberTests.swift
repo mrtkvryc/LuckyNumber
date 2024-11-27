963d12266a8a66b9cc94fa06447ab84fe34de45d
mrtkvryc <dwalin9696@gmail.com>
2024-11-27 08:53:17 +0300

    Initial Commit

---
+++ LuckyNumberTests/LuckyNumberTests.swift
@@ -0,0 +1,17 @@
+//
+//  LuckyNumberTests.swift
+//  LuckyNumberTests
+//
+//  Created by GTMAC15 on 27.11.2024.
+//
+
+import Testing
+@testable import LuckyNumber
+
+struct LuckyNumberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
