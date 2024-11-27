8e0a41ea0c5166d4d04fa1da175a10ff08b02a7a
mrtkvryc <dwalin9696@gmail.com>
2024-11-27 09:46:11 +0300

    Lucky number application. It suggests a number by pressing the button.

--- LuckyNumber/ViewController.swift
+++ LuckyNumber/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var luckyNumber: UILabel!
+    @IBAction func getLuckyNumber(_ sender: UIButton) {
+        luckyNumber.text = String(Int.random(in: 0..<10))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
